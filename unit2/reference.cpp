f7b7502da3008bcd58e2229b5b1d746b49daded4
rohitj <rjohri22@gmail.com>
2026-09-09 21:31:37 +0530

    have added modifytwovariables

---
+++ unit2/reference.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 10;
+
+    int &ref = x;
+
+    cout << "x = " << x << endl;
+    cout << "ref = " << ref << endl;
+
+    return 0;
+}
